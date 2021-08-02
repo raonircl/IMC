b1187829619711b3f9d04e1e6e2dd161c3a076ff
raonircl <raoni.afi@gmail.com>
2021-08-02 04:46:02 -0300

    Projeto IMC javascript

---
+++ script/imc.js
@@ -0,0 +1,34 @@
+const calcular = document.getElementById("calcular")
+
+function imc() {
+        const nome = document.getElementById("nome").value;
+            const altura = document.getElementById("altura").value;
+                const peso = document.getElementById("peso").value;
+                    const resultado = document.getElementById("resultado");
+        
+        
+        if (nome !== "" && altura !== "" && peso !== "") {
+            const valorImc = (peso / (altura * altura)).toFixed(1);
+            let classificacao = "";
+
+                if (valorImc < 18.5) {
+                    classificacao = "Abaixo do peso!";                    
+                }else if (valorImc < 25){
+                    classificacao = "Peso ideal. Parabéns!";
+                }else if(valorImc < 30){
+                    classificacao = "Está levemente acima do peso!";
+                }else if (valorImc < 35){
+                    classificacao = "Está com obesidade grau I";    
+                }else if (valorImc < 40){
+                    classificacao = "Está com obesidade grau II"    
+                }else {
+                    classificacao = "Está com obesidade grau III";    
+                }
+                resultado.textContent = `${nome} seu IMC é ${valorImc} e você está ${classificacao}`;    
+                
+        }else {
+            resultado.textContent = "Preencha todos os campos!"
+        }
+    } 
+    calcular.addEventListener("click", imc)
+  
